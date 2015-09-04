07a6df628914f50f22aadb4e4868ba9d6e67193f
Andrew Gross <apgross@udel.edu>
2015-09-04 19:42:42 -0400

    Update/Optimized

--- src/savingsCalculator/Main.java
+++ src/savingsCalculator/Main.java
@@ -11,36 +11,20 @@ public class Main {
 		
 		//Each block of code contain a single prompt and conversion into int value
 		//I'm wondering whether or not this can be consolidated via while/for loop...
-		String work_years_str;
 		System.out.println("How many years do you intend to work?");
-		work_years_str = (user_input.next());
-		int work_years;
-		work_years = Integer.parseInt(work_years_str);
+		int work_years = user_input.nextInt();
 		
-		String avg_return_str;
 		System.out.println("What is your average expected return on investment?");
-		avg_return_str = (user_input.next());
-		int avg_return;
-		avg_return = Integer.parseInt(avg_return_str);
-		
-		String draw_funds_str;
+		double avg_return = user_input.nextDouble();
+
 		System.out.println("How long do you intend to draw from Social Security?");
-		draw_funds_str = (user_input.next());
-		int draw_funds;
-		draw_funds = Integer.parseInt(draw_funds_str);
+		int draw_funds = user_input.nextInt();
 		
-		String req_incm_str;
 		System.out.println("What is your required income?");
-		req_incm_str = (user_input.next());
-		int req_incm;
-		req_incm = Integer.parseInt(req_incm_str);
+		int req_incm = (user_input.nextInt());
 		
-		String SSI_incm_str;
 		System.out.println("What is your expected Social Security income?");
-		SSI_incm_str = (user_input.next());
-		int SSI_incm;
-		SSI_incm = Integer.parseInt(SSI_incm_str);
-		
+		int SSI_incm = user_input.nextInt();
 
 	}
 
