7a638b5e5c2cf5cbdad78960b840f1b513f6fd86
Andrew Gross <apgross@udel.edu>
2015-09-06 16:56:15 -0400

    Added algorithm to calculate monthly savings goal.

--- src/savingsCalculator/Main.java
+++ src/savingsCalculator/Main.java
@@ -14,9 +14,12 @@ public class Main {
 		System.out.println("How many years do you intend to work?");
 		int work_years = user_input.nextInt();
 		
-		System.out.println("What is your average expected return on investment?");
-		double avg_return = user_input.nextDouble();
-
+		System.out.println("What is your average expected return on investment? (0-20%");
+		double avg_return_invest = user_input.nextDouble();
+		
+		System.out.println("What is your average expected return on paypack? (0-3%");
+		double avg_return_payback = user_input.nextDouble();
+		
 		System.out.println("How long do you intend to draw from Social Security?");
 		int draw_funds = user_input.nextInt();
 		
@@ -26,6 +29,12 @@ public class Main {
 		System.out.println("What is your expected Social Security income?");
 		int SSI_incm = user_input.nextInt();
 
+		double total_savings = (req_incm - SSI_incm) * 20;
+		double savings_retirement = total_savings/Math.pow(avg_return_payback, draw_funds);
+		double savings_invested = savings_retirement/Math.pow(avg_return_invest, work_years);
+		double monthly_savings = savings_invested/12;
+		System.out.println("You must save $" + monthly_savings + " per month to achieve your retirement goal.");
+		
 	}
 
 }
