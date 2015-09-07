370378201e1659a00653ea487668b17ea822c192
Andrew Gross <apgross@udel.edu>
2015-09-07 12:58:24 -0400

    Nearly complete

--- src/savingsCalculator/Main.java
+++ src/savingsCalculator/Main.java
@@ -15,10 +15,10 @@ public class Main {
 		int work_years = user_input.nextInt();
 		
 		System.out.println("What is your average expected return on investment? (0-20%");
-		double avg_return_invest = 1 + (user_input.nextDouble())/100;
+		double avg_return_invest = (user_input.nextDouble())/100;
 		
 		System.out.println("What is your average expected return on paypack? (0-3%)");
-		double avg_return_payback = 1 + (user_input.nextDouble())/100;
+		double avg_return_payback = (user_input.nextDouble())/100;
 		
 		System.out.println("How long do you intend to draw from Social Security?");
 		int draw_years = user_input.nextInt();
@@ -31,11 +31,14 @@ public class Main {
 		
 		user_input.close();
 
-		double total_savings = (req_incm - SSI_incm) * 20;
-		double savings_retirement = total_savings/Math.pow(avg_return_payback, draw_years);
-		double savings_invested = savings_retirement/Math.pow(avg_return_invest, work_years);
-		double monthly_savings = savings_invested/12;
-		System.out.printf("You must save $%.2f per month to achieve your retirement goal.", monthly_savings);
+		double total_savings = (req_incm - SSI_incm);
+		System.out.println("Total savings is " + total_savings);
+		double savings_retirement = total_savings * (1 - (1/(Math.pow((1+avg_return_payback/12), draw_years*12))))/(avg_return_payback/12); 
+		System.out.println("Savings retirement is " + savings_retirement);
+		double interest_multiplier = (Math.pow(1 + (avg_return_invest/12), work_years*12) - 1) / (avg_return_invest/12);
+		System.out.println("Interest multiplier is " + interest_multiplier);
+		double monthly_rate = savings_retirement/interest_multiplier;
+		System.out.printf("You must save $%.2f per month to achieve your retirement goal.", monthly_rate);
 		
 	}
 
